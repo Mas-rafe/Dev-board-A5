9edfb3f38b05b4ca66413d9e38d91addf485384d
Mas-rafe <eftemasrafe@gmail.com>
2025-03-03 22:51:00 +0600

    404 found

--- Html/javaScript/column1.js
+++ Html/javaScript/column1.js
@@ -1,7 +1,11 @@
 document.getElementById("btn-discover").addEventListener('click',function(event){
-    event.preventDefault;
-    console.log("done")
+    event.preventDefault();
     window.location.href = "./page2.html"
+})
+document.getElementById("btn-pTwo").addEventListener('click',function(event){
+    event.preventDefault();
+    console.log("done")
+    window.location.href = "./index.html"
 })
 
 document.getElementById("btn-1").addEventListener('click',function(event){
@@ -13,9 +17,19 @@ document.getElementById("btn-1").addEventListener('click',function(event){
     const sum = task - 1;
     document.getElementById("task-1").innerText = newSum;
     document.getElementById("task-2").innerText = sum;
+    const description = document.getElementById("description");
+    const desc1 = document.createElement('p');
+    desc1.innerText ='you have complete the task at';
+    desc1.style.border = "2px solid"
+    desc1.style.borderRadius = "10px"
+    desc1.style.textAlign = "center"
+    desc1.style.backgroundColor = "#F4F7FF"
+    desc1.style.margin = "10px"
+    description.append(desc1);
+    console.log(desc1)    
 })
 document.getElementById("btn-2").addEventListener('click',function(event){
-    event.preventDefault()
+    event.preventDefault() 
     alert("Board updated successfuly")
     const newTask = getInnerTextById("task-1");
     const newSum = newTask + 1;
