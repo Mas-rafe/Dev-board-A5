ed62e6ac4b057b884743240c4233829baccf3128
Mas-rafe <eftemasrafe@gmail.com>
2025-03-04 20:22:47 +0600

    whats the problem

--- javaScript/column1.js
+++ javaScript/column1.js
@@ -7,9 +7,11 @@ document.getElementById("btn-discover").addEventListener('click',function(event)
  //   console.log("done")
  //   window.location.href = "./index.html"
 //})
-document.getElementById("btn-1").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
+for(let i = 1; i<= 6;i++)
+document.getElementById(`btn-${i}`).addEventListener('click',function(event){
+    event.preventDefault();
+    console.log(i)
+    alert("Board updated successfuly");
     const newTask = getInnerTextById("task-1");
     const newSum = newTask + 1;
     const task = getInnerTextById("task-2");
@@ -24,111 +26,9 @@ document.getElementById("btn-1").addEventListener('click',function(event){
     desc1.style.textAlign = "center"
     desc1.style.backgroundColor = "#F4F7FF"
     desc1.style.margin = "10px"
+    desc1.style.padding = "10px"
     description.append(desc1);
     console.log(desc1)  
 })
     
-   document.getElementById("btn-2").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
-    const newTask = getInnerTextById("task-1");
-    const newSum = newTask + 1;
-    const task = getInnerTextById("task-2");
-    const sum = task - 1;
-    document.getElementById("task-1").innerText = newSum;
-    document.getElementById("task-2").innerText = sum;
-    const description = document.getElementById("description");
-    const desc1 = document.createElement('p');
-    desc1.innerText ='you have complete the task at';
-    desc1.style.border = "2px solid"
-    desc1.style.borderRadius = "10px"
-    desc1.style.textAlign = "center"
-    desc1.style.backgroundColor = "#F4F7FF"
-    desc1.style.margin = "10px"
-    description.append(desc1);
-    console.log(desc1)    
-})
-
-document.getElementById("btn-3").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
-    const newTask = getInnerTextById("task-1");
-    const newSum = newTask + 1;
-    const task = getInnerTextById("task-2");
-    const sum = task - 1;
-    document.getElementById("task-1").innerText = newSum;
-    document.getElementById("task-2").innerText = sum;
-    const description = document.getElementById("description");
-    const desc1 = document.createElement('p');
-    desc1.innerText ='you have complete the task at';
-    desc1.style.border = "2px solid"
-    desc1.style.borderRadius = "10px"
-    desc1.style.textAlign = "center"
-    desc1.style.backgroundColor = "#F4F7FF"
-    desc1.style.margin = "10px"
-    description.append(desc1);
-    console.log(desc1)    
-})
-
-document.getElementById("btn-4").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
-    const newTask = getInnerTextById("task-1");
-    const newSum = newTask + 1;
-    const task = getInnerTextById("task-2");
-    const sum = task - 1;
-    document.getElementById("task-1").innerText = newSum;
-    document.getElementById("task-2").innerText = sum;
-    const description = document.getElementById("description");
-    const desc1 = document.createElement('p');
-    desc1.innerText ='you have complete the task at';
-    desc1.style.border = "2px solid"
-    desc1.style.borderRadius = "10px"
-    desc1.style.textAlign = "center"
-    desc1.style.backgroundColor = "#F4F7FF"
-    desc1.style.margin = "10px"
-    description.append(desc1);
-    console.log(desc1)    
-})
-
-document.getElementById("btn-5").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
-    const newTask = getInnerTextById("task-1");
-    const newSum = newTask + 1;
-    const task = getInnerTextById("task-2");
-    const sum = task - 1;
-    document.getElementById("task-1").innerText = newSum;
-    document.getElementById("task-2").innerText = sum;
-    const description = document.getElementById("description");
-    const desc1 = document.createElement('p');
-    desc1.innerText ='you have complete the task at';
-    desc1.style.border = "2px solid"
-    desc1.style.borderRadius = "10px"
-    desc1.style.textAlign = "center"
-    desc1.style.backgroundColor = "#F4F7FF"
-    desc1.style.margin = "10px"
-    description.append(desc1);
-    console.log(desc1)    
-})
-
-document.getElementById("btn-6").addEventListener('click',function(event){
-    event.preventDefault()
-    alert("Board updated successfuly")
-    const newTask = getInnerTextById("task-1");
-    const newSum = newTask + 1;
-    const task = getInnerTextById("task-2");
-    const sum = task - 1;
-    document.getElementById("task-1").innerText = newSum;
-    document.getElementById("task-2").innerText = sum;
-    const description = document.getElementById("description");
-    const desc1 = document.createElement('p');
-    desc1.innerText ='you have complete the task at';
-    desc1.style.border = "2px solid"
-    desc1.style.borderRadius = "10px"
-    desc1.style.textAlign = "center"
-    desc1.style.backgroundColor = "#F4F7FF"
-    desc1.style.margin = "10px"
-    description.append(desc1);
-    console.log(desc1)    
-})
+  
